079edea12ae375b8b63869bced4892bb7ae6b526
niahon <niahon3107@gmail.com>
2024-09-06 10:21:09 -0300

    add turn functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.addEventListener ("DOMContentLoaded", () => {
     let elLightPieces = document.querySelectorAll('.piece.light');
+    let elDarkPieces = document.querySelectorAll('.piece.dark');
     for (element of elLightPieces) {
-        element.addEventListener("dragstart", dragstartHandler);
+        element.addEventListener("dragstart", lightdragstartHandler);
+    }
+    for (element of elDarkPieces) {
+        element.addEventListener("dragstart", darkdragstartHandler);
     }
     let elTile = document.querySelectorAll('.tile.dark');
     for (element of elTile) {
@@ -10,9 +14,28 @@ window.addEventListener ("DOMContentLoaded", () => {
     }
 })
 
-function dragstartHandler(e) {
-    e.dataTransfer.setData("text/html", e.target.id)
-    e.dataTransfer.effectAllowed = "move";
+let playCount = 0;
+
+function lightdragstartHandler(e) {
+    console.log(playCount);
+    if (playCount % 2 === 0) {
+        e.dataTransfer.setData(`text/html`, e.target.id)
+        e.dataTransfer.effectAllowed = "move";
+    }
+    else {
+        return;
+    }
+}
+
+function darkdragstartHandler(e) {
+    console.log(playCount);
+    if (playCount % 2 === 1) {
+        e.dataTransfer.setData(`text/html`, e.target.id)
+        e.dataTransfer.effectAllowed = "move";
+    }
+    else {
+        return;
+    }
 }
 
 function dragoverHandler(e) {
@@ -22,6 +45,21 @@ function dragoverHandler(e) {
 
 function dropHandler(e) {
     e.preventDefault();
+    if (this.children.length > 0) {
+        return;
+    }
     let data = e.dataTransfer.getData("text/html");
     e.target.appendChild(document.getElementById(data));
+
+    turnChange();
+}
+
+function turnChange() {
+    playCount ++;
+    let turnText = document.getElementById('turn');
+    if (playCount % 2 === 0) {
+        turnText.textContent = "White's turn";
+    } else {
+        turnText.textContent = "Black's turn";
+    }
 }
